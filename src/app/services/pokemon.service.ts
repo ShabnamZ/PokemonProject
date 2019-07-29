51e304e0f94246de936a092bd4d81ffc8f4545eb
aknight9255 <amanda.knight55@gmail.com>
2019-07-29 16:22:48 -0400

    phase one service layer

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth-service';
 import { HttpHeaders } from'@angular/common/http';
+import { Pokemon} from '../model/pokemon';
 import {Observable } from 'rxjs/Observable';
 
 const Api_Url = 'https://pokeapi.co/docs/v2.html/#pokemon';
@@ -17,8 +18,16 @@ export class PokemonService {
     return this._http.get<Pokemon[]>(`${Api_Url}/Pokemon`, {headers: this.getHeaders() });
   }
 
+  getPokemon(id: string) {
+    return this._http.get(`${Api_Url}/Pokemon/${id}`), {header: this.getHeaders() };
   }
+
+
+  private getHeaders(){
+    return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
+  }
+}
   
 
   
-}
+
