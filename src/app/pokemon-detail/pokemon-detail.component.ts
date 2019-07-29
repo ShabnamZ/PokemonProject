0a83e4b68be89c977423d2d639c138cad49fe83f
masongarard <masongarard@gmail.com>
2019-07-29 16:43:53 -0400

    model

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Pokemon } from '../pokemon';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Pokemon } from '../model/pokemon';
+ 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
   styleUrls: ['./pokemon-detail.component.css']
 })
 export class PokemonDetailComponent implements OnInit {
-
+  @Input() pokemon: Pokemon;
+ 
   constructor() { }
-
+ 
   ngOnInit() {
   }
-
+ 
 }
