bd4b9866fd3625a9b14c8da63cda357beccb726b
ShabnamZ <shabnam.ziyovuddinova@gmail.com>
2019-07-29 17:02:56 -0400

    navbar changed

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,24 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AuthService } from './auth-service';
-import { HttpHeaders } from'@angular/common/http';
-import {Observable } from 'rxjs/Observable';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { AuthService } from './auth-service';
+// import { HttpHeaders } from'@angular/common/http';
+// import {Observable } from 'rxjs/Observable';
 
-const Api_Url = 'https://pokeapi.co/docs/v2.html/#pokemon';
+// const Api_Url = 'https://pokeapi.co/docs/v2.html/#pokemon';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PokemonService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class PokemonService {
 
-  constructor(private _http: HttpClient, private _authService: AuthService) { }
+//   constructor(private _http: HttpClient, private _authService: AuthService) { }
 
-  getPokemons(): Observable<Pokemon[]>{
-    return this._http.get<Pokemon[]>(`${Api_Url}/Pokemon`, {headers: this.getHeaders() });
-  }
+//   getPokemons(): Observable<Pokemon[]>{
+//     return this._http.get<Pokemon[]>(`${Api_Url}/Pokemon`, {headers: this.getHeaders() });
+//   }
 
-  }
+//   }
   
 
   
-}
+// }
