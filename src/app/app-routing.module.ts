58f600972162314381c8e693d8bf0b4533c48ef5
masongarard <masongarard@gmail.com>
2019-07-29 20:52:16 -0400

    imtired

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AboutComponent} from './about/about.component';
 import {HomeComponent} from './home/home.component';
-import {ProductComponent} from './product/product.component';
+import {PokemonIndexComponent} from './pokemonindex/pokemonindex.component';
 
 
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'product', component:ProductComponent},
+  {path: 'product', component:PokemonIndexComponent},
 ];
 
 @NgModule({
---
+++ src/app/pokemonindex/pokemonindex.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+import { Pokemon } from '../model/pokemon';
+
+@Component({
+  selector: 'app-pokemonindex',
+  templateUrl: './pokemonindex.component.html',
+  styleUrls: ['./pokemonindex.component.css']
+})
+export class PokemonIndexComponent implements OnInit {
+
+  pokemons: Pokemon[] = [];
+  // columnNames = ['ID', 'Name', 'Height'];  
+
+  constructor(private _pokemonService: PokemonService) { }
+
+  ngOnInit() {
+    this.getPokemon(); 
+  }
+  getPokemon() {
+    this._pokemonService.getPokemons().subscribe((poke => {
+      console.log(poke);
+      this.pokemons = poke.results;
+    }));
+    console.log(this.pokemons);
+  }
+
+}
