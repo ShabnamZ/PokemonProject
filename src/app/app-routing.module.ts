3c7dfae3f12da4dfccc85c4ecfa863b4d9215123
aknight9255 <amanda.knight55@gmail.com>
2019-07-30 10:10:16 -0400

    updated details view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {AboutComponent} from './about/about.component';
 import {HomeComponent} from './home/home.component';
 import {PokemonIndexComponent} from './pokemonindex/pokemonindex.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+
 
 
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'home', component: HomeComponent},
   {path: 'product', component:PokemonIndexComponent},
+  {path: 'detail', component:PokemonDetailComponent}
 
 ];
 
